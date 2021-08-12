09824a656c3d2ae874fe8f72014a4f6e9a60ac36
unknown <turetcan@gmail.com>
2021-08-12 11:46:50 +0300

    webpack proxy

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -1,58 +1,58 @@
 import React from 'react';
 import axios from 'axios';
 
-class UserSignupPage extends React.Component{
-    state = {
-        username: null,
-        displayName: null,
-        password: null,
-        passwordRepeat: null
-      };
-    
-      onChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-          [name]: value
-        });
-      };
+class UserSignupPage extends React.Component {
+  state = {
+    username: null,
+    displayName: null,
+    password: null,
+    passwordRepeat: null
+  };
 
-      onClickSignup = event => {
-          event.preventDefault();
+  onChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
 
-          const {username,displayName,password}=this.state;
+  onClickSignup = event => {
+    event.preventDefault();
 
-          const body = {
-              username,
-              displayName,
-              password
-          };
-          axios.post('http://localhost:8080/api/1.0/users',body)
-      };
+    const { username, displayName, password } = this.state;
 
-    render() {
-        return (
-            <form>
-              <h1>Sign Up</h1>
-              <div>
-                <label>Username</label>
-                <input name="username" onChange={this.onChange} />
-              </div>
-              <div>
-                <label>Display Name</label>
-                <input name="displayName" onChange={this.onChange} />
-              </div>
-              <div>
-                <label>Password</label>
-                <input name="password" type="password" onChange={this.onChange} />
-              </div>
-              <div>
-                <label>Password Repeat</label>
-                <input name="passwordRepeat" type="password" onChange={this.onChange} />
-              </div>
-              <button onClick={this.onClickSignup}>Sign Up</button>
-            </form>
-          );
-    }
+    const body = {
+      username,
+      displayName,
+      password
+    };
+    axios.post('/api/1.0/users', body)
+  };
+
+  render() {
+    return (
+      <form>
+        <h1>Sign Up</h1>
+        <div>
+          <label>Username</label>
+          <input name="username" onChange={this.onChange} />
+        </div>
+        <div>
+          <label>Display Name</label>
+          <input name="displayName" onChange={this.onChange} />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" onChange={this.onChange} />
+        </div>
+        <div>
+          <label>Password Repeat</label>
+          <input name="passwordRepeat" type="password" onChange={this.onChange} />
+        </div>
+        <button onClick={this.onClickSignup}>Sign Up</button>
+      </form>
+    );
+  }
 }
 
 export default UserSignupPage;
